a282110dec435fd3d7eb21c174334d4828c71a09
jrricklis@gmail.com <jrricklis@gmail.com@eaec55f4-2234-1352-8801-9fa1b99acc44>
2014-10-21 00:57:06 +0000

    Started work on MainActivity and TabBar

--- trunk/ group-project-se --username ChrisKelly1219@gmail.com/app/build/generated/source/r/debug/com/example/chris/group_project/R.java
+++ trunk/ group-project-se --username ChrisKelly1219@gmail.com/app/build/generated/source/r/debug/com/example/chris/group_project/R.java
@@ -10,30 +10,53 @@ package com.example.chris.group_project;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int tab_bar_button_background=0x7f040000;
+        public static final int tab_bar_button_selected_background=0x7f040001;
+    }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int phoneNumberSize=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090008;
+        public static final int editText=0x7f090002;
+        public static final int pager=0x7f090000;
+        public static final int phoneNumberString=0x7f090001;
+        public static final int section_label=0x7f090003;
+        public static final int show_contact=0x7f090004;
+        public static final int tab_button_image=0x7f090006;
+        public static final int tab_button_indicator=0x7f090005;
+        public static final int tab_button_label=0x7f090007;
     }
     public static final class layout {
-        public static final int activity_my=0x7f030000;
+        public static final int activity_main=0x7f030000;
+        public static final int activity_my=0x7f030001;
+        public static final int fragment_main=0x7f030002;
+        public static final int show_contact=0x7f030003;
+        public static final int tab_bar=0x7f030004;
+        public static final int tab_bar_button=0x7f030005;
     }
     public static final class menu {
-        public static final int my=0x7f070000;
+        public static final int main=0x7f080000;
+        public static final int my=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_main=0x7f060003;
+        public static final int title_section1=0x7f060004;
+        public static final int title_section2=0x7f060005;
+        public static final int title_section3=0x7f060006;
     }
     public static final class style {
         /**  Customize your theme here. 
          */
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
     }
 }

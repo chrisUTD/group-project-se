35433a261ad23ae29fde6a36d92c5e9d1b4baba3
jrricklis@gmail.com <jrricklis@gmail.com@eaec55f4-2234-1352-8801-9fa1b99acc44>
2014-10-28 13:04:18 +0000

    Edited Contact and ContactManager to load from the Android database. Created a list view to display the contacts that are loaded.

---  group-project-se --username ChrisKelly1219@gmail.com/app/src/main/java/com/example/chris/group_project/TabBar.java
+++  group-project-se --username ChrisKelly1219@gmail.com/app/src/main/java/com/example/chris/group_project/TabBar.java
@@ -3,13 +3,17 @@ package com.example.chris.group_project;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.view.View.OnClickListener;
 
 /**
  * Created by Jonny on 10/20/14.
  */
-public class TabBar extends LinearLayout {
+public class TabBar extends LinearLayout implements OnClickListener {
+
+    private TabBarAdapter adapter;
 
     public TabBar(Context context){
         super(context);
@@ -20,14 +24,31 @@ public class TabBar extends LinearLayout {
         super(context, attrs);
     }
 
-    public TabBar(Context context, ViewGroup parent, TabBarAdapter delegate){
+    public TabBar(Context context, ViewGroup parent, TabBarAdapter adapter){
         super(context);
+        this.adapter = adapter;
+
         ViewGroup tabBarView = (ViewGroup)LayoutInflater.from(context).inflate(R.layout.tab_bar, parent, false);
+
+        Integer count = adapter.getCount();
+        for (Integer i = 0; i < count; i++){
+            TabBarButton button = adapter.getTabBarButtonForIndex(i);
+            button.setTag(i);
+            button.setOnClickListener(this);
+            tabBarView.addView(button);
+        }
 
         parent.addView(tabBarView);
     }
 
     public void setSelected(Integer index){
 
+    }
+
+    public void onClick(View target){
+        Integer tag = (Integer)target.getTag();
+        if (tag != null){
+            adapter.tabButtonClicked(tag);
+        }
     }
 }

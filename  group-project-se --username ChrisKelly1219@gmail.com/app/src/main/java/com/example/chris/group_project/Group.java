eaf4fda97762c860608fcb8854da983f92fc6077
jrricklis@gmail.com <jrricklis@gmail.com@eaec55f4-2234-1352-8801-9fa1b99acc44>
2014-10-31 05:19:33 +0000

    Began work on Group classes to add loading from database. Added ability to launch display_contact via Intent with specified contact, and wired that up to launch from the main contact list view.

---  group-project-se --username ChrisKelly1219@gmail.com/app/src/main/java/com/example/chris/group_project/Group.java
+++  group-project-se --username ChrisKelly1219@gmail.com/app/src/main/java/com/example/chris/group_project/Group.java
@@ -1,6 +1,7 @@
 package com.example.chris.group_project;
 
 
+import java.util.ArrayList;
 
 /**
  * Created by nahuecht on 10/20/2014.
@@ -9,10 +10,24 @@ package com.example.chris.group_project;
 public class Group
 {
     private int id;
+
     private String name;
+    /**
+     * Array that holds Contact objects for the purpose of storing references to Android managed
+     * contacts. Those references are stored in the ContactToGroup table and when the GroupManager
+     * creates the groups, it loads those references so that Contacts that have been added to each
+     * group can be retrieved from the ContactManager, which separately loads its contacts from
+     * the Android ContactsProvider
+     */
+    private ArrayList<String> CONTACT_IDs;
+
+    public Group(){
+        this.CONTACT_IDs = new ArrayList<String>();
+    }
 
     public Group(int id, String name)
     {
+        this();
         this.id = id;
         this.name = name;
     }
@@ -35,5 +50,9 @@ public class Group
     public void setName(String name)
     {
         this.name = name;
+    }
+
+    public ArrayList<String> getCONTACT_IDs(){
+        return CONTACT_IDs;
     }
 }

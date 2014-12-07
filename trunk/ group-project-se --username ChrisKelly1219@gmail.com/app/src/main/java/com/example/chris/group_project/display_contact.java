736113e74c80046289c3307b115e0b525dfa74c5
ChrisKelly1219@gmail.com <ChrisKelly1219@gmail.com@eaec55f4-2234-1352-8801-9fa1b99acc44>
2014-12-07 03:12:03 +0000

    better commenting

--- trunk/ group-project-se --username ChrisKelly1219@gmail.com/app/src/main/java/com/example/chris/group_project/display_contact.java
+++ trunk/ group-project-se --username ChrisKelly1219@gmail.com/app/src/main/java/com/example/chris/group_project/display_contact.java
@@ -18,18 +18,55 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+/**
+ * display_contact class for displaying the contact information when a contact is selected
+ */
 
     public class display_contact  extends Activity
     {
+        /**
+         * optionsMenu holds the options to delete or block/unBlock a contact
+         */
         Menu optionsMenu;
+
+        /**
+         * This is populated with views that contain a Text and Call button as well as a phone
+         * number string
+         */
         ListView phoneListView;
+        /**
+         * This is populated with views that contain an button as well as an email string
+         */
         ListView emailListView;
+        /**
+         * contact is passed in by an intent nd is then assigned in the onCreate method
+         */
         Contact contact;
+        /**
+         * Holds the image URI for showing the contact's assigned picture
+         */
         ImageView picture;
+        /**
+         * Holds a string containing the contact's display name
+         */
         TextView name;
+        /**
+         * Contains a an array of Strings of the contact's phone numbers
+         */
         ArrayAdapter<String> phoneNumberAdapter;
+        /**
+         * Contains a an array of Strings of the contact's email addresses
+         */
         ArrayAdapter<String> emailAdapter;
+        /**
+         * The layout contains button's that are assigned to different groups
+         */
         LinearLayout groupHolder;
+        /**
+         * This contains the contact's number in the android database
+         * this is used when the contact is edited and allows the display to update
+         * with any changed information
+         */
         private static int cNum;
 
 
@@ -39,7 +76,12 @@ import java.util.ArrayList;
         public display_contact(){}
 
 
-        // Populates views and sets contact
+        /**
+         *
+         * This method is run on the activity creation
+         * populates various views of the contact
+         *
+         */
         @Override
         protected void onCreate(Bundle savedInstanceState)
         {
@@ -49,9 +91,10 @@ import java.util.ArrayList;
             populateItems();
         }
 
-
-        // Retrieve the contact id passed through the intent when this activity is launched
-        // the retrieve contact and set it as this.contact
+        /**
+         *  Retrieve the contact id passed through the intent when this activity is launched
+         * the id is used to set this.contact to the appropriate value
+         */
         private void setContact()
         {
             Intent intent = getIntent();
@@ -72,6 +115,9 @@ import java.util.ArrayList;
             }
         }
 
+        /**
+         * Inflates the NameView,ImageView,and the PhoneNumber and Email Address list views
+         */
         private void populateItems()
         {
             createNameView();
@@ -82,25 +128,30 @@ import java.util.ArrayList;
             populateContactGroupView();
         }
 
-        // Sets nmae view to the name of the contact
+        /**
+         * Sets sc_Name to the display name of contact
+         */
         private void createNameView()
         {
             name = (TextView) findViewById(R.id.sc_Name);
             name.setText(contact.getDisplayName());
         }
 
-        // Sets the image view on show_COntact to the assigned picture for the contact
+        /**
+         * Sets the image view on show_Contact to the assigned picture for the contact
+         */
         private void createImageView()
         {
             picture = (ImageView) findViewById(R.id.sc_Image);
-
             if(contact.getPhotoUri() != null) {
                 picture.setImageURI(Uri.parse(contact.getPhotoUri()));
             }
         }
 
-        //Creates the edit button for the contact and adds an onClickListener
-        // that initiates the edit contact activity
+        /**
+         * Creates the edit button for the contact and adds an onClickListener
+         * that initiates the edit contact activity
+         */
         private void createEditButton()
         {
             cNum = contact.getId();
@@ -110,23 +161,35 @@ import java.util.ArrayList;
             {
                 public void onClick(View temp)
                 {
-                    Uri lookupUri = ContactsContract.Contacts.getLookupUri(Long.parseLong(contact.getCONTACT_ID()), contact.getLOOKUP_KEY());
+                    Uri lookupUri = ContactsContract.Contacts.getLookupUri
+                    (Long.parseLong(contact.getCONTACT_ID()), contact.getLOOKUP_KEY());
+
                     Intent editIntent = new Intent(Intent.ACTION_EDIT);
+
                     editIntent.setDataAndType(lookupUri, ContactsContract.Contacts.CONTENT_ITEM_TYPE);
                     editIntent.putExtra("finishActivityOnSaveCompleted", true);
+
                     startActivityForResult(editIntent,0);
                 }
             });
 
         }
-        //Populates the phone_number_list view by using the PhoneListAdapter
+
+        /**
+         * Populates the phoneListView using the phoneNumberListAdapter
+         * The views contain a string and two buttons
+         */
         private void CreatePhoneListView()
         {
             phoneListView = (ListView) findViewById(R.id.phone_number_listView);
             phoneNumberAdapter = new phoneNumberListAdapter();
             phoneListView.setAdapter((phoneNumberAdapter));
         }
-        //Populates the email_list_view by using the emailListAdapter
+
+        /**
+         * Populates the email_list_view by using the emailListAdapter
+         * the views contain a string and one button
+         */
         private void CreateEmailListView()
         {
             emailListView = (ListView) findViewById(R.id.email_list_view);
@@ -134,6 +197,11 @@ import java.util.ArrayList;
             emailListView.setAdapter((emailAdapter));
         }
 
+        /**
+         * Populates the contactGroupView with buttons
+         * These buttons display a group name and have an onClickListener
+         * that launches a group activity
+         */
        private void populateContactGroupView()
        {
 
@@ -141,7 +209,11 @@ import java.util.ArrayList;
            addGroupButtonsToView(groups);
        }
 
-        //This method gets the name of all groups belonging to contact and puts them on buttons
+        /**
+         *
+         * @return ArrayList of Buttons that display the groups name and have an onClickListener
+         * which launches the Group Activity
+         */
         private ArrayList<Button> createGroupBtns()
         {
             ArrayList<Button> groupBs = new ArrayList<Button>();
@@ -155,8 +227,13 @@ import java.util.ArrayList;
             }
             return groupBs;
         }
-        //Creates the button with size characteristics and colors
-        //These buttons are then passed back to createGroupBtns where they are added tpo an array
+
+        /**
+         * Creates the button with size characteristics and colors
+         * These buttons are then passed back to createGroupBtns where they are added to an array
+         * @param group
+         * @return a button with a custom color and size
+         */
         private Button createGroupButton(Group group)
         {
             Button btn = new Button(this);
@@ -168,13 +245,18 @@ import java.util.ArrayList;
             return btn;
         }
 
-        //adds the listener to each group button that launches the appropriate group activity
-        //when the group is tapped
+        /**
+         * Adds a listener to each group button
+         * this listener starts the Group Activity for the specific group when tapped
+         * @param btn Button
+         * @param temp Group
+         * @return A button with with an onClickListener
+         */
         private Button addGroupClickListener(Button btn,Group temp)
         {
 
             final GroupManager manager = GroupManager.getInstance(this);
-            final Group group =  manager.get(temp.getId()); // make sure the reference is correct
+            final Group group =  manager.get(temp.getId());         // makes sure the reference is correct
             btn.setOnClickListener( new View.OnClickListener()
             {
                 public void onClick(View view)
@@ -182,7 +264,7 @@ import java.util.ArrayList;
 
                     if (group != null )
                     {
-                        Intent displayGroupIntent = new Intent( display_contact.this,GroupActivity.class);
+                     Intent displayGroupIntent = new Intent( display_contact.this,GroupActivity.class);
 
                         if (group != null) {
                             displayGroupIntent.putExtra("groupId", group.getId());
@@ -194,8 +276,13 @@ import java.util.ArrayList;
             return btn;
         }
 
-        //Creates individual views that are then added to a horizontal list view
-        //The larger list view is contained in a horizontal scroll view
+        /**
+         * Creates individual views that only contain a button
+         * these views are then added to a horizontal list view
+         * The larger list view is contained in a horizontal scroll view
+         * This larger list view is designed to hold all group buttons
+         * @param groups
+         */
         private void addGroupButtonsToView(ArrayList<Button> groups)
         {
             groupHolder = (LinearLayout) findViewById(R.id.display_groups);
@@ -212,19 +299,33 @@ import java.util.ArrayList;
                         new TextView(this),
                         new ViewGroup.LayoutParams(5,ViewGroup.LayoutParams.WRAP_CONTENT)
                 );
-
             }
         }
 
-
+        /**
+         * This class is used to make views that are added to a list view
+         * the individual views contain a string and two buttons
+         * This class is for populating phoneListView
+         */
         private class phoneNumberListAdapter extends ArrayAdapter<String>
         {
-
+            /**
+             * calls super() to set information from ArrayAdapter
+             */
             public phoneNumberListAdapter()
             {
                 super (display_contact.this, R.layout.phone_number_list_view, contact.getPhoneNumbers());
             }
 
+            /**
+             * This method adds a string and two buttons to a linear layout
+             * The buttons are used for texting and calling respectively
+             * It also adds an onClicklistener to each button to complete the view
+             * @param position
+             * @param view
+             * @param parent
+             * @return
+             */
             @Override
             public View getView(int position, View view, ViewGroup parent)
             {
@@ -261,21 +362,35 @@ import java.util.ArrayList;
 
                    }
                 });
-
-
-
                 return view;
             }
 
         }
 
+        /**
+         * This class is used to make views that are added to a list view
+         * the individual views contain a string and one buttons
+         * This class is for populating emailListView
+         */
         private class emailListAdapter extends ArrayAdapter<String>
         {
+            /**
+             * calls super() to set information from ArrayAdapter
+             */
             public emailListAdapter()
             {
                 super (display_contact.this, R.layout.email_list_view, contact.getEmailAddresses());
             }
 
+            /**
+             * Creates a layout that contains a string and one button
+             * the string contains a n email address and the button has an onClickListerner
+             * the listener allows a user to email when tapped
+             * @param position
+             * @param view
+             * @param parent
+             * @return
+             */
             @Override
             public View getView(int position, View view, ViewGroup parent)
             {
@@ -305,7 +420,12 @@ import java.util.ArrayList;
 
         }
 
-        // Inflate the menu; this adds items to the action bar if it is present.
+        /**
+         *
+         * Inflate the menu; this adds items to the action bar if it is present.
+         * @param menu
+         * @return a menu that allows user to block or delete a contact
+         */
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -315,6 +435,9 @@ import java.util.ArrayList;
             return true;
         }
 
+        /**
+         * Sets it so when a block contact calls it goes straight to voice mail instead of ringing
+         */
         private void SetBlockMessage()
         {
             if(contact != null && this.optionsMenu != null)
@@ -331,6 +454,11 @@ import java.util.ArrayList;
             }
         }
 
+        /**
+         * Does the appropriate action when an item is selected from the options menu
+         * @param item
+         * @return true when the action has been completed
+         */
         @Override
         public boolean onOptionsItemSelected(MenuItem item)
         {
@@ -359,6 +487,10 @@ import java.util.ArrayList;
             }
         }
 
+        /**
+         * This function repopulates all views after editing a few in the android default
+         * contact editor
+         */
         protected void onRestart()
         {
             super.onRestart();
